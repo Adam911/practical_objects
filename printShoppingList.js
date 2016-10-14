e14375043ba22c0e2318c4b2e62e887cad61e15e
Dwayne <saulsdwayne@gmail.com>
2016-10-14 11:42:25 +0200

    Finished the first exercise of object

---
+++ printShoppingList.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+
+
+var shoppingList = [{
+  itemName: 'Bread',
+  price: 11.00
+}, {
+  itemName: 'Milk',
+  price: 7.00
+}, {
+  itemName: 'Bread',
+  price: 19.50
+}];
+
+console.log('ShoppingList: ');
+for (var i = 0; i < shoppingList.length; i++) {
+  var listItems = shoppingList[i];
+  assert(listItems.itemName +  " @ " + listItems.price);
+
+  console.log(listItems.itemName +  " @ " +  listItems.price);
+
+}
