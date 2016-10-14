9ad11fb6a270670ddc8ad2cd7fff5b58f3827d4e
Dwayne <saulsdwayne@gmail.com>
2016-10-14 11:18:00 +0200

    completed the examples,starting excercise

---
+++ person.js
@@ -0,0 +1,25 @@
+var shoppingList = [
+    { itemName : 'Milk 500ml', qty : 2},
+    { itemName : 'Bread', qty : 1},
+    { itemName : 'Rolls', qty : 6},
+    { itemName : 'Fanta 1L', qty : 2},
+];
+
+//add a new object to a list like this
+shoppingList.push({
+    itemName : 'Coke 1L',
+    qty : 3});
+
+// or like this
+
+// var barOne = {
+//     itemName : 'Bar One',
+//     qty : 3};
+//
+// shoppingList.push(barOne);
+
+console.log('Shopping list: ');
+for(var i=0;i<shoppingList.length;i++){
+    var listItem = shoppingList[i];
+    console.log(listItem.itemName + " " + listItem.qty);
+}
