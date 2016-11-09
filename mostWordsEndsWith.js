d0df62d73127637beea29c68dc4380d86dd41d95
Dwayne <saulsdwayne@gmail.com>
2016-11-09 15:27:18 +0200

    Just now did the fruitsold for in loop

--- mostWordsEndsWith.js
+++ mostWordsEndsWith.js
@@ -1,12 +1,8 @@
 var sentence = 'Down by the river there is a man that quiver and shiver, but he needs to deliver a packet that he think is a big racket and a packet of of gum.'
 
-var splits = sentence.split(' ',31)
-var lastLetter = 't';
-
 function mostWordsEndsWith (){
-for (var i = 0; i < wordsGathered.length; i++) {
-  var totalLastLetter =  wordsGathered[i].startWith(lastLetter)
-}
+    for (var i = 0; i < words.length; i++) {
+      words[i]
     }
 
-mostWordsEndsWith(sentence);
+}
