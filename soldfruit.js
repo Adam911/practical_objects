ba936e905e10300a6ef76dce1da28ace9b9f7d83
Dwayne <saulsdwayne@gmail.com>
2016-11-09 15:26:23 +0200

    Just now did the fruitsold for in loop

--- soldfruit.js
+++ soldfruit.js
@@ -1,13 +1,26 @@
 var fruitString = 'Banana,Banana,Pear,Orange,Apple,Melon,Grape,Apple,Banana,Grape,Melon,Grape,Melon,Apple,Grape,Banana,Orange,Melon,Orange,Banana,Banana,Orange,Pear,Grape,Orange,Orange,Apple,Apple,Banana'
-var fruitList = fruitString.split(',');
+
+var fruitList = fruitString.split(',')
 var fruitMap = {};
+var high = 0;
 
-for (var i = 0; i < fruitList.length; i++) {
-  var currentFruit = fruitList[i];
-
-  if (fruitMap[currentFruit] === undefined) {
-    fruitMap[currentFruit] = 0;
-  }
-  fruitMap[currentFruit] = fruitMap[currentFruit] + 1
+   function soldfruits(){
+    for (var i = 0; i < fruitList.length; i++) {
+        var currentFruit = fruitList[i]
+    if (fruitMap[currentFruit] === undefined) {
+         fruitMap[currentFruit]= 0;
+        }
+        fruitMap[currentFruit] = fruitMap[currentFruit] +1
+    }
+    console.log(fruitMap);
 }
-console.log(fruitMap);
+
+
+soldfruits(fruitMap);
+
+for (var fruits in fruitMap) {
+  if (fruitMap[fruits] > high ) {
+    high = fruitMap[fruits]
+  }
+}
+console.log(high);
